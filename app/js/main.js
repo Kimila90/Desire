fb9bbc93c504e06a537295b2a8a09f57209135a3
Kimila <irinamakincian@yandex.ru>
2021-08-02 15:07:12 +0300

    added menu, footer and content for first screen

---
+++ app/js/main.js
@@ -0,0 +1,36 @@
+$(function () {
+
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true
+    });
+
+
+//     $('.header__btn').on('click', function () {
+//         $('.rightside-menu').removeClass('rightside-menu--close');
+//         console.log("DELETE");
+//     });
+
+//     $('.rightside-menu__close').on('click', function () {
+//         $('.rightside-menu').addClass('rightside-menu--close');
+//         console.log("ADD");
+//     });
+
+})
+const headerMenuButtonOpen = document.querySelector('.header__btn'); // кнопка на хедере для открытия
+const rightSideMenuButtonClose = document.querySelector('.rightside-menu__close'); // кнопка в меню для закрытия
+const rightSideMenu = document.querySelector('.rightside-menu'); // само меню
+
+let showRightSideMenu = () => { // показать меню (удалит класс, который его скрывал)
+    rightSideMenu.classList.remove('rightside-menu--close');
+};
+
+let hideRightSideMenu = () => { // скрывает меню (доабвит класс который его скроет)
+    rightSideMenu.classList.add('rightside-menu--close');
+};
+
+headerMenuButtonOpen.addEventListener("click", showRightSideMenu); // на событе click вызывает функцию "ПОКАЗА"
+rightSideMenuButtonClose.addEventListener("click", hideRightSideMenu); // на событе click вызывает функцию "СКРЫТИЯ"
+
